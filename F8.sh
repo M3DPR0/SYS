838150d5ca5253e448b4303930cbe271549af4d5
Susanto <50844272+M3DPR0@users.noreply.github.com>
2019-09-15 14:24:05 +0700

    Create F8.sh

---
+++ F8.sh
@@ -0,0 +1,13 @@
+pkg install lolcat
+pkg install cosway
+cowsay -f eyes "MedPro | Here For God" | lolcat   
+echo ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■ | lolcat -a -d 7
+figlet -f standard "SOESANTO" | lolcat
+
+echo ---------------------------------- | lolcat -a -d 2
+echo Welcome Soesanto, Please Foccus... | lolcat -a -d 20
+echo ---------------------------------- | lolcat -a -d 2
+
+echo --------------------------- | lolcat -a -d 2
+date | lolcat
+echo --------------------------- | lolcat -a -d 2
